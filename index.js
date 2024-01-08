a14272dd72900d31435e8624a97f440f3a732d98
AntCannon <anthonycannonier@gmail.com>
2024-01-07 22:39:58 -0500

    pass addPieceToChessBoard, lab complete

--- index.js
+++ index.js
@@ -85,14 +85,17 @@ function dynamicCheckerBoard(rows = 4, cols = 4) {
   return checkerBoard;
 }
 
+console.log(dynamicCheckerBoard(4,4));
 
 /**
  * Create an array of square objects that represent a chessboard. Please see instructions for more details
  * @return {Object[]} An array of square objects that have the following properties: piece, positionX, positionY, and color.
  */
 function createChessBoard() {
-  // create board coordinate alpha reference array
-  // const boardAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  // // BONUS: create board coordinate reference arrays
+  // const boardAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  // const boardNums = [ 8, 7, 6, 5, 4, 3, 2, 1];
+
   const chessBoard = [];
   // iterate through height of board
   for (let i = 0; i < 8; i++) {
@@ -105,7 +108,7 @@ function createChessBoard() {
         piece: null,
         positionX: i,
         positionY: j,
-        // squareName: `${boardAlpha[i]}${j+1}`,
+        // squareName: `${boardAlpha[i]}${boardNums[j]}`, // BONUS
         color: ((i + j) % 2) ? 'dark' : 'light', 
       };
       // push space object to boardRow;
@@ -125,7 +128,32 @@ function createChessBoard() {
  * @return {Object{}}  The modified board array of objects.
  */
 function addPieceToChessBoard(piece, row, column) {
-  return 
+  // // BONUS: create board coordinate alpha reference array
+  // const boardAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  // const boardNums = [ 8, 7, 6, 5, 4, 3, 2, 1];
+
+  const chessBoard = [];
+  // iterate through height of board
+  for (let i = 0; i < 8; i++) {
+    // create new empty boardrow for each iteration
+    let boardRow = [];
+    // iterate through spaces
+    for (let j = 0; j < 8; j++) {
+      // create new empty space object
+      let boardSpace = {
+        piece: null,
+        positionX: i,
+        positionY: j,
+        // squareName: `${boardAlpha[i]}${boardNums[j]}`, // BONUS
+        color: ((i + j) % 2) ? 'dark' : 'light', 
+      };
+      boardRow.push(boardSpace);      
+    }
+    // push completed boardRow to chessBoard
+    chessBoard.push(boardRow);
+  }
+  chessBoard[row][column].piece = piece
+  return chessBoard;
 }
 
 module.exports = {
