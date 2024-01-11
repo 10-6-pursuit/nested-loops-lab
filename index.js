c8482679e75c4ccb2b5aa45a141c1d8e0e013caa
gerardjr42 <ggarciajr12@gmail.com>
2024-01-10 23:54:57 -0500

    completed createChessBoard

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function getAllAlbumTitles(artists) {
   let artistAlbum = [];
   for (let data of artists) {
     for (let albums of data.albums) {
-      artistAlbum.push(albums.title)
+      artistAlbum.push(albums.title);
     }
   }
   return artistAlbum;
@@ -51,7 +51,7 @@ function getAllSongs(artists) {
  * @return {string} a string of spaces and # that represent a checkerboard that is 8 x 8.
  */
 function simpleCheckerBoard() {
-  const str = " # # # #\n# # # # \n"
+  const str = " # # # #\n# # # # \n";
   const checkerboard = str.repeat(4);
   return checkerboard;
 }
@@ -62,13 +62,49 @@ function simpleCheckerBoard() {
  * @param {number} [cols = 4] - An integer that represents the number of columns to create.
  * @return {string} a string of spaces and # that represent a checkerboard that has the appropriate number of rows on columns based on the parameters passed.
  */
-function dynamicCheckerBoard() {}
+
+//pseudocode:
+/*
+ */
+function dynamicCheckerBoard(row = 4, column = 4) {
+  const arr = [];
+  for (let i = 0; i < row; i++) {
+    let position = "";
+    for (let j = 0; j < column; j++) {
+      position += (i + j) % 2 === 1 ? "#" : " "; 
+    }
+    arr.push(position);
+  }
+  return arr.join("\n") + "\n";
+}
 
 /**
  * Create an array of square objects that represent a chessboard. Please see instructions for more details
  * @return {Object[]} An array of square objects that have the following properties: piece, positionX, positionY, and color.
  */
-function createChessBoard() {}
+function createChessBoard(row = 8, column = 8) {
+
+
+  let chestboard = [];
+
+  for (let i = 0; i < row; i++) {
+    let row = [];
+    
+    for (let j = 0; j < column; j++) {
+      let square = {
+        piece: null,
+        positionX: i,
+        positionY: j,
+        color:(i + j) % 2 === 1 ? "dark" : "light",
+      };
+      row.push(square);
+    }
+    chestboard.push(row);
+  }
+  return chestboard;
+}
+
+
 
 /**
  * Update a square on the board to have a chess piece "on" it
@@ -77,7 +113,11 @@ function createChessBoard() {}
  * @param {number} column - An array of guest objects. See the instructions and tests for a full breakdown of what is in each guest object.
  * @return {Object{}}  The modified board array of objects.
  */
-function addPieceToChessBoard(piece, row, column) {}
+function addPieceToChessBoard(piece, row, column) {
+  // const chessboard = createChessBoard();  
+  //   chessboard[row][column].piece = piece;
+  //   return chessboard;
+}
 
 module.exports = {
   getAllArtistNames,
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+let square = {
+  //   piece: null,
+  //   positionX: null,
+  //   positionY: null,
+  //   color: ""
+  // };
+
+  // let row = 8;
+  // let column = 8;
+  
+  // for (let i = 0; i < row; i++) {
+  //   let position = [];
+  //   for (let j = 0; j < column; j++) {
+  //     position.push(square)
+  //   }
+  //   arr.push(square);
+  // }
